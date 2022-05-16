013eaf658d9f461a998b5b64ba4b3b66482c6ad5
kirVish <prprchvish@gmail.com>
2022-05-16 20:26:05 +0300

    REACT-RENDER-AND-EFFECTS

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,14 @@
+import React, { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
-
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [data, setData] = useState<number>(-1);
+    useEffect(() => {
+        const fnc = (data: number) => setData(data);
+        subscribe(props.sourceId, fnc);
+        return () => {
+            unsubscribe(props.sourceId, fnc);
+            setData(-1);
+        };
+    }, [props.sourceId]);
+    return <div>{`${props.sourceId}: ${data}`}</div>;
 }
